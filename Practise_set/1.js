84a57ec2305699c1bbd2814527ed1c4019edc4de
Gurdev Singh <>
2022-08-06 08:59:31 +0000

    new

---
+++ Practise_set/1.js
@@ -0,0 +1,20 @@
+//q1
+let a="You are a great team player"
+a+=1
+console.log(a)
+
+//q2
+console.log(typeof (a))
+
+//q3
+const data1={
+  name:"Taish",
+  height: -4,
+  issweet:true,
+}
+console.log(data1)
+//data1=Gur
+
+//q4
+data1["friend"]= "sukhman"
+console.log(data1)
---
+++ Tutorial/objects.js
@@ -0,0 +1,7 @@
+const item= {
+  "Gurdev":true,
+  "sharn":58,
+  "harsh":undefined,
+  "Taish":"Chhoti Bachi Ho Kya"
+}
+console.log(item)
